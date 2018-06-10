f76198d071982655aea8d6a0ac41abf6d712a5e3
Adisorn Artwichai <art@kkumail.com>
2018-06-10 19:07:08 +0700

    Add files via upload

---
+++ messages/th/menu.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 19/10/2560
+ * Time: 21:36
+ */
+
+return [
+    'Home' => 'หน้าหลัก',
+    'Manage Student Info' => 'จัดการข้อมูลนักเรียน',
+    'Candidates List' => 'รายชื่อผู้สมัคร',
+    'Students List' => 'รายชื่อนักเรียนทุน',
+    'Out of scholarships' => 'นักเรียนที่พ้นสภาพทุน',
+    'Scholarship Costs' => 'ค่าใช้จ่ายนักศึษาทุน',
+    'Public relations' => 'ประชาสัมพันธ์',
+    'Manage News' => 'จัดการข่าวประชาสัมพันธ์',
+    'Appointment Calendar' => 'ปฏิทินนัดหมาย',
+    'Setting' => 'การตั้งค่า',
+    'Manage Scholarships' => 'จัดการทุนการศึกษา',
+    'Manage Acitivity' => 'จัดการกิจกรรม',
+    'Assign Committee' => 'แต่งตั้งกรรมการ',
+
+    'Scholarship Costs' => 'เงินทุนที่ได้รับ',
+    'Present Progressive' => 'นำเสนอความก้าวหน้าทุน',
+    'Portfolio-Project' => 'ผลงาน-การแข่งขัน',
+    'Manage Portfolio' => 'จัดการผลงาน',
+    'Manage Project' => 'จัดการโครงงาน-งานวิชาการ',
+    'Manage Activity' => 'จัดการกิจกรรม',
+
+];
+
+
+?>
